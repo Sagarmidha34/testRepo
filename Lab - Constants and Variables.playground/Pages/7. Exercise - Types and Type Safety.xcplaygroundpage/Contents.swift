6512d2b5567e06df06c2e4cf256ee039aec7abbf
Sagarmidha34 <sagar1197.be23@chitkara.edu.in>
2025-07-09 10:39:56 +0530

    constants and variables
    lab practice of constant and variables

---
+++ Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+## Exercise - Types and Type Safety
+ 
+ Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
+ */
+var firstDecimal  = 10.2
+var secondDecimal = 10.5
+
+//:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+var trueOrFalse = true
+print("as the type assigned to it was double value and it can not be boolean now ")
+
+//:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+var str = "hello"
+print("type annotation assigned to it was different")
+
+//:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+var number = 10
+print("still datatype assigned to each was different ")
+
+/*:
+[Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
+ */
